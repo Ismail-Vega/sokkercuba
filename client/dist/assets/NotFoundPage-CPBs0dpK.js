7c4954d6112b8c786efe6b89f5aafa8363bc6914
Ismail Vega <ismail.softing@gmail.com>
2024-06-26 08:43:46 -0300

    chore: add dist directory as static to the repo
    Signed-off-by: Ismail Vega <ismail.softing@gmail.com>

---
+++ client/dist/assets/NotFoundPage-CPBs0dpK.js
@@ -0,0 +1,1 @@
+import{j as o}from"./vendor-ClnM7L9C.js";const d=()=>o.jsx("div",{children:"Page not found"});export{d as default};
---
+++ client/dist/assets/ScrollToTopOnMount-YbRgBmSJ.js
@@ -0,0 +1,1 @@
+import{r as o}from"./vendor-ClnM7L9C.js";function r(){return o.useEffect((()=>{window.scrollTo(0,0)}),[]),null}export{r as S};
